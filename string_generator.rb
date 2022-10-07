5908ab213f12cccc9ffb22d47d6b75a4076d00b7
Fonzie! <40026023+Fonstw@users.noreply.github.com>
2022-10-07 11:07:20 +0200

    Add files via upload

--- string_generator.rb
+++ string_generator.rb
@@ -1,10 +1,13 @@
-length = ARGV[0] ? ARGV[0].to_i : 32
+length = ARGV[0] ? ARGV[0].to_i : 16
+alphabet = ARGV[1] ? ARGV[1].chars : nil
 
-# Fully random, any printable ASCII character
-random_string = (0...length).map { (rand 32..126).chr }.join
-puts random_string
-
-# With a given alphabet
-alphabet = ARGV[1] || "0123456789ABCDEF"
-random_string = (0...length).map { alphabet.chars.shuffle.first }.join
-puts random_string
+# If no alphabet
+if alphabet == nil
+  # Fully random, any printable ASCII character
+  random_string = (0...length).map { (rand 32...127).chr }.join
+  puts random_string
+else
+  # With a given alphabet
+  random_string = (0...length).map { alphabet.sample }.join
+  puts random_string
+end
